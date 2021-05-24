9cb8c4fd517fb6656a5ad9e1204de60aee650a07
karinkicks <KRot5601>
2021-05-24 14:56:01 +0300

    Миграция

---
+++ product-server/src/main/resources/db/migration/V1__create.sql
@@ -0,0 +1,5 @@
+create table products (
+                          id                      bigserial primary key,
+                          name                   varchar(100),
+                          price                   bigint
+);
